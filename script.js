3feec1720a45c4ef3e0be174e89ace1557d845c6
kenjitagawa <kenjitagawa.kt@gmail.com>
2021-05-05 06:36:27 -0400

    Update on style and Javascript

--- script.js
+++ script.js
@@ -11,31 +11,53 @@ const cardsBack = document.querySelectorAll(".card__back");
 console.log(cardsBack);
 
 // "More" button
-const btnsMore = document.querySelectorAll(".card__more");
+const btnsMore = document.querySelectorAll(".card__front");
 console.log(btnsMore);
 
 // "Less" button
 const btnsClose = document.querySelectorAll(".card__close");
 console.log(btnsClose);
 
+// cards.forEach((e)=> {
+//     console.log(e)
+// })
 
 for (let i = 0; i < cards.length; i++) {
-    let btnClose = btnsClose[i];
-    let btnMore = btnsMore[i];
+    // Getting individual button
+    const btnMore = btnsMore[i];
+    const btnClose = btnsClose[i];
+
+    // Getting individual front and back pannels
+    const front = cardsFront[i];
+    const back = cardsBack[i];
+
+    btnMore.addEventListener("click", (e) => {
+        front.classList.add('hidden');
+        back.classList.remove('hidden');
+    })
+
+    btnClose.addEventListener("click", (e) => {
+        front.classList.remove('hidden');
+        back.classList.add('hidden');
+    })
 }
 
 // Adding event listeners
-btnsMore.forEach((e) => {
-    e.addEventListener('click', (e) => {
-        console.log(e);
-    })
-});
+// for (let i = 0; i < cards.length; i++) {
 
-btnsClose.forEach((e) => {
-    e.addEventListener('click', (e) => {
-        console.log(e);
-    })
-});
+// };
+
+// btnsMore.forEach((e) => {
+//     e.addEventListener('click', (e) => {
+//         console.log(e);
+//     })
+// });
+
+// btnsClose.forEach((e) => {
+//     e.addEventListener('click', (e) => {
+//         console.log(e);
+//     })
+// });
 
 
 
